63c12de9ca41312dbcd6bf3fc0e904f65d5225a5
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-02 17:45:33 +0300

    Created User CRUD Service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  findAll() {
+    return this.repo.find({});
+  }
+
+  async findOne(id: number) {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  async updateOne(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) throw new NotFoundException('User not found');
+
+    // return this.repo.update(id, attrs);
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async deleteOne(id: number) {
+    const user = await this.findOne(id);
+    if (!user) throw new NotFoundException('User not found');
+
+    return this.repo.remove(user);
+  }
+
+  async deleteAll() {
+    return this.repo.clear();
+  }
+}
