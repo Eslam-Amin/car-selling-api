04a102883ce3facc009f51a94a3fa1cf3a248f91
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-02 23:38:11 +0300

    Added Expose to the User dto

--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -1,8 +1,16 @@
+import { Expose } from 'class-transformer';
+
 export class UserDto {
+  @Expose()
   id: number;
+  @Expose()
   email: string;
+  @Expose()
   username: string;
+  @Expose()
   firstName: string;
+  @Expose()
   lastName: string;
+  @Expose()
   fullName: string;
 }
