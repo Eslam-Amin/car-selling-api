5bb4c3a4782c486bfc888a4fa0b5961ad6ecfd1c
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-02 17:36:09 +0300

    Created Signup service function and its controller function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from '../users/dtos/create-user.dto';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dtos/login.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from 'src/users/dtos/user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto) {
+    const user = await this.authService.signup(
+      body.email,
+      body.password,
+      body.username,
+      body.firstName,
+      body.lastName,
+    );
+    return {
+      message: 'User created successfully',
+      data: user,
+    };
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../users/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  async signup(
+    email: string,
+    password: string,
+    username: string,
+    firstName: string,
+    lastName: string,
+  ) {
+    const existingUser = await this.repo.findOne({
+      where: [{ email }, { username }],
+    });
+    if (existingUser?.email === email)
+      throw new BadRequestException('Email in use');
+    else if (existingUser?.username === username)
+      throw new BadRequestException('Username in use');
+    const user = this.repo.create({
+      email,
+      password,
+      username,
+      firstName,
+      lastName,
+    });
+    const savedUser = await this.repo.save(user);
+    return { ...savedUser, password: undefined };
+  }
+}
