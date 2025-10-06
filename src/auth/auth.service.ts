99d2716125eaec72f08af0405f677d66c5e945f8
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-06 14:20:04 +0300

    Used the emailService to send Emails in signup and created a new verify service function to compare the code with the provided code

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,12 +8,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 import * as bcrypt from 'bcryptjs';
 import { UsersService } from 'src/users/users.service';
+import { EmailService } from 'src/email/email.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     @InjectRepository(User) private repo: Repository<User>,
+    private emailService: EmailService,
   ) {}
 
   async signup(
@@ -31,12 +33,16 @@ export class AuthService {
     else if (existingUser?.username === username)
       throw new BadRequestException('Username in use');
     const hashedPassword = await bcrypt.hash(password, 10);
+    const code = Math.floor(100000 + Math.random() * 900000).toString();
+    await this.emailService.sendVerificationEmail(email, code);
+    const hashedCode = await bcrypt.hash(code, 10);
     return this.usersService.createOne(
       email,
       hashedPassword,
       username,
       firstName,
       lastName,
+      hashedCode,
     );
   }
 
@@ -48,6 +54,20 @@ export class AuthService {
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) throw new NotFoundException('Invalid credentials');
 
+    if (!user.verified) throw new BadRequestException('User not verified');
+
     return user;
+  }
+
+  async verify(email: string, code: string) {
+    const user = await this.repo.findOne({
+      where: { email },
+    });
+    if (!user) throw new NotFoundException('Invalid credentials');
+    const isCodeValid = await bcrypt.compare(code, user.code as string);
+    if (!isCodeValid) throw new BadRequestException('Invalid code');
+    user.verified = true;
+    user.code = null;
+    return this.repo.save(user);
   }
 }
