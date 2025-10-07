dd163d2f818b4742c25c66bb1af804d49a142a9f
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-07 16:25:12 +0300

    Added cookie session to the main project file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // This is a global pipe that will validate the request body
+  app.use(
+    cookieSession({
+      keys: ['testing'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Remove properties that aren't in the DTO
