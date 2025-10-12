d36e64345bdcf907bd67c0dcbf85801afddba5a3
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-13 02:55:25 +0300

    Updated import statements

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -6,7 +6,7 @@ import {
   UpdateDateColumn,
   ManyToOne,
 } from 'typeorm';
-import { User } from 'src/users/user.entity';
+import { User } from '../users/user.entity';
 
 @Entity()
 export class Report {
