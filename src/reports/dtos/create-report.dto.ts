f12e7889fe4c00f3a678a1083081266ba456f545
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-13 01:05:14 +0300

    Created create-report dto

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,36 @@
+import {
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+  IsLongitude,
+  IsLatitude,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Max(2030)
+  year: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+}
