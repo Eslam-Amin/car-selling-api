18f3bb7c8af73d74e1f5c4ccbc10760bc4f8e43a
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-13 02:09:29 +0300

    Associate the user with the created Report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -14,6 +14,8 @@ import {
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
 
 @Controller('reports')
 @UseGuards(AuthGuard)
@@ -21,8 +23,8 @@ export class ReportsController {
   constructor(private reportsService: ReportsService) {}
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.createOne(body);
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.createOne(body, user);
   }
 
   @Get()
