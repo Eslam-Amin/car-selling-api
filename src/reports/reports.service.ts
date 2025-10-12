24402d0088468592309778208e3d24636f34672a
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-13 01:05:55 +0300

    Created Report Service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  createOne(reportDto: CreateReportDto) {
+    const report = this.repo.create(reportDto);
+    return this.repo.save(report);
+  }
+
+  async findAll(skip: number, limit: number) {
+    const reportsCount = await this.repo.count();
+    const reports = await this.repo.find({ skip, take: limit });
+    return { reports, reportsCount };
+  }
+
+  async findOne(id: number) {
+    const report = await this.repo.findOneBy({ id });
+    if (!report) throw new NotFoundException('Report not found');
+    return report;
+  }
+
+  async updateOne(id: number, reportDto: CreateReportDto) {
+    const report = await this.repo.findOneBy({ id });
+    if (!report) throw new NotFoundException('Report not found');
+    Object.assign(report, reportDto);
+    return this.repo.save(report);
+  }
+}
