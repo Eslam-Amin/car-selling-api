9912c9ce2a3663b7f0746e72ac984ac3b266ea77
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-13 02:55:44 +0300

    Serialize the output

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -13,12 +13,15 @@ import {
 } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-report.dto';
-import { AuthGuard } from 'src/guards/auth.guard';
-import { CurrentUser } from 'src/users/decorators/current-user.decorator';
-import { User } from 'src/users/user.entity';
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ReportDto } from './dtos/report.dto';
 
 @Controller('reports')
 @UseGuards(AuthGuard)
+@Serialize(ReportDto)
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
   @Post()
