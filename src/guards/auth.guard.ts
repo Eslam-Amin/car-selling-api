8c19878ab16b9c5d4718c32dc8a20830fac4a782
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-13 01:04:55 +0300

    Get instance of userService to get the user whom logged in to check if he still exists

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,12 +1,22 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { UsersService } from 'src/users/users.service';
 
 export class AuthGuard implements CanActivate {
-  canActivate(
+  constructor(private userService: UsersService) {}
+  async canActivate(
     context: ExecutionContext,
     // ): boolean | Promise<boolean> | Observable<boolean> {
   ) {
     const request = context.switchToHttp().getRequest();
+    const { userId } = request.session;
+    if (!userId) return false;
+    const user = await this.userService.findOne(userId, false);
+    if (!user) {
+      request.session.destroy?.();
+      request.session.userId = null;
+      return false;
+    }
     return request.session.userId;
   }
 }
