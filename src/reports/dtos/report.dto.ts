f98693ea0dc18c51c0d5202ee6dcd6c39dc6e616
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-13 03:28:52 +0300

    Created A report dto to be used in the serializer

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,27 @@
+import { Expose, Transform } from 'class-transformer';
+import { User } from '../../users/user.entity';
+export class ReportDto {
+  @Expose()
+  id: number;
+  @Expose()
+  price: number;
+  @Expose()
+  make: string;
+  @Expose()
+  model: string;
+  @Expose()
+  year: number;
+  @Expose()
+  mileage: number;
+  @Expose()
+  lng: number;
+  @Expose()
+  lat: number;
+  @Transform(({ obj }) => obj?.user?.id)
+  @Expose()
+  user: number;
+  @Expose()
+  createdAt: string;
+  @Expose()
+  updatedAt: string;
+}
