d4a263ab670f92dbc3ccdd51ebb9c15622bb745d
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-14 01:38:49 +0300

    Inject UsersStartupService to users providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,12 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { UserStartupService } from './user-startup.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UserStartupService],
   exports: [UsersService],
 })
 export class UsersModule {}
