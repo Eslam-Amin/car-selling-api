9ce65bfd9cbcda0c835a20932c32033c736e7e64
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-14 01:43:12 +0300

    configure the currentUser middleware

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,19 @@
-import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 import { UsersService } from '../users/users.service';
 import { EmailService } from '../email/email.service';
-import { CurrentUserInterceptor } from '../users/interceptors/current-user.interceptor';
+import { CurrentUserMiddleware } from '../users/middlewares/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [
-    AuthService,
-    UsersService,
-    EmailService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor,
-    },
-  ],
+  providers: [AuthService, UsersService, EmailService],
 })
-export class AuthModule {}
+export class AuthModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
